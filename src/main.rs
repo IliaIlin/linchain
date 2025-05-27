2979513d43c66f0e97e7fceaa61cc3cef32b259f
IliaIlin <ilya-ilin-777@mail.ru>
2025-05-27 18:04:04 +0200

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod lib;
+
+use std::{collections::HashMap, sync::mpsc::{self, Receiver}};
+
+use crate::lib::{InMemoryChannelNetwork, Peer};
+
 fn main() {
-    println!("Hello, world!");
+    let (tx1, rx1) = mpsc::channel();
+    let (tx2, rx2) = mpsc::channel();
+    let (tx3, rx3) = mpsc::channel();
+
+    let network_of_first_peer = InMemoryChannelNetwork {
+        incoming: rx1,
+        outgoing: HashMap::from([(2, tx2), (3, tx3.clone())]),
+    };
+    let network_of_second_peer = InMemoryChannelNetwork {
+        incoming: rx2,
+        outgoing: HashMap::from([(1, tx1), (3, tx3.clone())]),
+    };
+    let firstPeer = Peer {
+        id: 1,
+        network: network_of_first_peer,
+    };
+    firstPeer.run();
 }
