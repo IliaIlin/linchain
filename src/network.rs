1423a6e56e58f544c60452caae37bed8aa0638c2
IliaIlin <ilya-ilin-777@mail.ru>
2025-06-08 15:40:41 +0200

    Rewrite network to libp2p

--- src/network.rs
+++ src/network.rs
@@ -1,10 +1,16 @@
 use crate::blockchain::{SignedBlock, SignedTransaction};
 use crate::peer::PeerID;
+use libp2p::gossipsub::{Behaviour, Config, MessageAuthenticity};
+use libp2p::swarm::NetworkBehaviour;
+use libp2p::{Swarm, gossipsub, mdns, noise, tcp, yamux};
+use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::error::Error;
 use std::sync::mpsc::{Receiver, RecvError, SendError, Sender};
+use std::time::Duration;
 use thiserror::Error;
 
-#[derive(PartialEq, Debug, Clone)]
+#[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
 pub enum Message {
     ClientTransaction(SignedTransaction),
     PeerTransaction(SignedTransaction),
@@ -58,6 +64,62 @@ impl InMemoryChannelNetwork {
         }
         errors
     }
+}
+
+#[derive(NetworkBehaviour)]
+#[behaviour(to_swarm = "NetworkEvent")]
+pub struct P2PMdnsNetwork {
+    pub pub_sub: gossipsub::Behaviour,
+    pub mdns: mdns::tokio::Behaviour,
+}
+
+#[derive(Debug)]
+pub enum NetworkEvent {
+    Mdns(mdns::Event),
+    GossipSub(gossipsub::Event),
+    Application(Message),
+}
+
+impl From<mdns::Event> for NetworkEvent {
+    fn from(event: mdns::Event) -> Self {
+        NetworkEvent::Mdns(event)
+    }
+}
+impl From<gossipsub::Event> for NetworkEvent {
+    fn from(event: gossipsub::Event) -> Self {
+        NetworkEvent::GossipSub(event)
+    }
+}
+
+impl From<Message> for NetworkEvent {
+    fn from(msg: Message) -> Self {
+        NetworkEvent::Application(msg)
+    }
+}
+
+pub fn build_p2p_network_swarm(
+    stay_alive_secs: u64,
+) -> Result<Swarm<P2PMdnsNetwork>, Box<dyn Error>> {
+    let swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|key| {
+            let pub_sub =
+                Behaviour::new(MessageAuthenticity::Signed(key.clone()), Config::default())
+                    .expect("Can't create gossipsub behaviour");
+            let mdns =
+                mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())?;
+            Ok(P2PMdnsNetwork { pub_sub, mdns })
+        })?
+        .with_swarm_config(|cfg| {
+            cfg.with_idle_connection_timeout(Duration::from_secs(stay_alive_secs))
+        })
+        .build();
+    Ok(swarm)
 }
 
 #[cfg(test)]
